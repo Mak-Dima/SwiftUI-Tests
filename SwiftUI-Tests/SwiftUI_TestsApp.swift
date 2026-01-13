fb3745eb22f6041ae2f435abeb028e19f9a89f5d
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-01-12 18:14:19 -0600

    Initial Commit

---
+++ SwiftUI-Tests/SwiftUI_TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TestsApp.swift
+//  SwiftUI-Tests
+//
+//  Created by Dmytro Maksiutenko on 2026-01-12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-TestsTests/SwiftUI_TestsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TestsTests.swift
+//  SwiftUI-TestsTests
+//
+//  Created by Dmytro Maksiutenko on 2026-01-12.
+//
+
+import Testing
+@testable import SwiftUI_Tests
+
+struct SwiftUI_TestsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
