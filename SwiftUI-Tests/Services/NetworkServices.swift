9c2a559208a845aefedb345798fb0ca3b877a67d
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-01-17 12:22:18 -0600

    HTTPService crated.

--- SwiftUI-Tests/Services/NetworkServices.swift
+++ SwiftUI-Tests/Services/NetworkServices.swift
@@ -11,6 +11,23 @@ protocol NetworkService {
     func fetchData() async throws -> Data
 }
 
+final class HTTPService: NetworkService {
+    
+    func fetchData() async throws -> Data {
+        if let url = URL(string: "http://localhost:8080/default/read") {
+            let (data, response) = try await URLSession.shared.data(from: url)
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                throw NSError(domain: "Wrong status code", code: 0)
+            }
+            
+            return data
+        }
+       
+        throw NSError(domain: "Invalid URL string", code: 0)
+    }
+}
+
 final class MockHTTPService: NetworkService {
     
     var data: Data
